a22bcf07581a00274588a3d32c7f8d582bf04afc
Tim <vunvun0213@gmail.com>
2022-10-24 23:49:25 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while (num * (num + 1) / 2 < 105)
+            {
+                num++;
+
+            }
+            Console.WriteLine(num - 1);
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+		static void Main(string[] args)
+		{
+			int num = 300;
+			int i = 1;
+			while (i <= 300)
+			{
+				if (num % i == 0)
+				{
+					Console.WriteLine(i);
+
+				}
+				i++;
+			}
+		}
+    }
+}
